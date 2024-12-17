83d7ee2ccf2ff35f171dca2b0901d6cfbcb55610
Ganten Xu <zjxu97@gmail.com>
2024-12-17 16:42:29 +0800

    feat: add chart

--- src/main/java/com/gantenx/util/TradingChart.java
+++ src/main/java/com/gantenx/util/TradingChart.java
@@ -6,6 +6,8 @@ import org.jfree.chart.ChartFactory;
 import org.jfree.chart.ChartPanel;
 import org.jfree.chart.JFreeChart;
 import org.jfree.chart.annotations.XYPointerAnnotation;
+import org.jfree.chart.axis.DateAxis;
+import org.jfree.chart.axis.NumberAxis;
 import org.jfree.chart.plot.PlotOrientation;
 import org.jfree.chart.plot.XYPlot;
 import org.jfree.data.xy.XYSeries;
@@ -13,12 +15,15 @@ import org.jfree.data.xy.XYSeriesCollection;
 import org.jfree.ui.ApplicationFrame;
 
 import java.awt.*;
+import java.text.SimpleDateFormat;
 import java.util.List;
 
 public class TradingChart extends ApplicationFrame {
 
     public TradingChart(String title, List<RSI> rsiList, List<Order> orderList) {
         super(title);
+
+        // Create the RSI series
         XYSeries rsiSeries = new XYSeries("RSI");
         for (RSI rsi : rsiList) {
             rsiSeries.add(rsi.getOpenTime(), rsi.getRsi());
@@ -33,11 +38,14 @@ public class TradingChart extends ApplicationFrame {
         // Create the series collection
         XYSeriesCollection dataset = new XYSeriesCollection();
         dataset.addSeries(kLineSeries);
-        dataset.addSeries(rsiSeries);
 
-        // Create the chart
+        // Create the RSI dataset
+        XYSeriesCollection rsiDataset = new XYSeriesCollection();
+        rsiDataset.addSeries(rsiSeries);
+
+        // Create the chart for K-Line
         JFreeChart chart = ChartFactory.createXYLineChart(
-                title,
+                "K-Line and RSI Chart",
                 "Time",
                 "Price",
                 dataset,
@@ -45,13 +53,33 @@ public class TradingChart extends ApplicationFrame {
                 true, true, false
         );
 
-        // Customize the chart to add order markers
+        // Get the plot and configure the time axis
         XYPlot plot = chart.getXYPlot();
+
+        // Create a DateAxis for the time axis
+        DateAxis timeAxis = new DateAxis("Time");
+        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        timeAxis.setDateFormatOverride(format);
+        plot.setDomainPannable(true); // Make it pannable
+        plot.setDomainCrosshairVisible(true);
+        plot.setDomainPannable(true);
+        plot.setRangePannable(true);
+
+        plot.setDomainAxis(timeAxis);
+
+        // Create and add the secondary axis for RSI using NumberAxis
+        NumberAxis rsiAxis = new NumberAxis("RSI");
+        rsiAxis.setRange(0.0, 600.0);  // RSI range between 0 and 100
+        plot.setRangeAxis(1, rsiAxis);
+        plot.setDataset(1, rsiDataset);
+        plot.mapDatasetToRangeAxis(1, 1); // Map RSI dataset to the second Y axis
+
+        // Customize the chart to add order markers
         for (Order order : orderList) {
             // Place markers for buy/sell orders
             Color color = order.getType().equals("buy") ? Color.GREEN : Color.RED;
             XYPointerAnnotation xyPointerAnnotation = new XYPointerAnnotation(
-                    order.getType(),
+                    order.getType() + ":" + order.getPrice(),
                     order.getTimestamp(),
                     order.getPrice(),
                     0.0
