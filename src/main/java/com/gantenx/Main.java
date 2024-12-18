8c028469e15625327b712709e8f0a42c7819553f
Ganten Xu <zjxu97@gmail.com>
2024-12-18 15:46:40 +0800

    feat: add SQQQ Kline

--- src/main/java/com/gantenx/Main.java
+++ src/main/java/com/gantenx/Main.java
@@ -1,11 +1,21 @@
 package com.gantenx;
 
+import com.gantenx.model.Kline;
 import com.gantenx.strategy.QQQStrategy;
+import com.gantenx.util.CsvUtils;
+import com.gantenx.util.JsonUtils;
 import lombok.extern.slf4j.Slf4j;
+
+import java.util.List;
 
 @Slf4j
 public class Main {
     public static void main(String[] args) {
-        QQQStrategy.replay("20211112", "20221014");
+        // QQQStrategy.replay("20230106", "20241210");
+
+        List<Kline> sqqqKlineList = CsvUtils.getKLineFromCsv("data/SQQQ.csv", "20230106", "20241210");
+        for (Kline kline : sqqqKlineList) {
+            log.info(JsonUtils.toJson(kline));
+        }
     }
 }
